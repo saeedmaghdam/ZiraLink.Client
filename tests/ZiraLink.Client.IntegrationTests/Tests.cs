1dff1bd55dcc1572634ddb231892f2a66a6edd01
saeedmaghdam <smasafat@gmail.com>
2023-08-26 07:49:29 +0200

    added test container

---
+++ tests/ZiraLink.Client.IntegrationTests/Tests.cs
@@ -0,0 +1,39 @@
+﻿using DotNet.Testcontainers.Builders;
+
+namespace ZiraLink.Client.IntegrationTests
+{
+    public class Tests
+    {
+        [Fact]
+        [Trait("type", "integration")]
+        public async Task TestContainer()
+        {
+            // Create a new instance of a container.
+            var container = new ContainerBuilder()
+              // Set the image for the container to "testcontainers/helloworld:1.1.0".
+              .WithImage("testcontainers/helloworld:1.1.0")
+              // Bind port 8080 of the container to a random port on the host.
+              .WithPortBinding(8080, true)
+              // Wait until the HTTP endpoint of the container is available.
+              .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort(8080)))
+              // Build the container configuration.
+              .Build();
+
+            // Start the container.
+            await container.StartAsync()
+              .ConfigureAwait(false);
+
+            // Create a new instance of HttpClient to send HTTP requests.
+            var httpClient = new HttpClient();
+
+            // Construct the request URI by specifying the scheme, hostname, assigned random host port, and the endpoint "uuid".
+            var requestUri = new UriBuilder(Uri.UriSchemeHttp, container.Hostname, container.GetMappedPublicPort(8080), "uuid").Uri;
+
+            // Send an HTTP GET request to the specified URI and retrieve the response as a string.
+            var guid = await httpClient.GetStringAsync(requestUri)
+              .ConfigureAwait(false);
+
+            Assert.True(Guid.TryParse(guid, out _));
+        }
+    }
+}
